34deea81ccd0c4ce3c49dd16c7aef672208684f8
Volodymyr Petrunin <petruninvolodymyr@gmail.com>
2024-02-20 19:42:11 +0100

    add stats view

--- src/main/java/org/game/rps/controller/PlayerController.java
+++ src/main/java/org/game/rps/controller/PlayerController.java
@@ -1,9 +1,13 @@
 package org.game.rps.controller;
 
+import org.game.rps.domain.Statistic;
 import org.game.rps.domain.dto.RequestPlayerDto;
 import org.game.rps.service.PlayerService;
+import org.game.rps.service.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -26,5 +30,14 @@ public class PlayerController {
     public String registerPlayer(@ModelAttribute RequestPlayerDto dto) {
         playerService.createPlayer(dto);
         return "redirect:/login";
+    }
+
+    @GetMapping("/statistic")
+    public String checkStatistic(@AuthenticationPrincipal UserDetailsImpl userDetails, Model model) {
+        Statistic statistic = userDetails.getPlayer().getStatistic();
+
+        model.addAttribute("statistic", statistic);
+
+        return "my-statistic";
     }
 }
